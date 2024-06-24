e280295740e823d59d957322d47602550d68ee57
陈贤杰 <chenxianjie@onemt.com.cn>
2024-06-24 16:29:24 +0800

    Init

---
+++ Assets/HotUpdate/Hello.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+
+public class Hello
+{
+    public static void Run()
+    {
+        Debug.Log("Hello, TESTTTTTT!!!!!");
+        GameObject go = new GameObject("Test1");
+    }
+}
